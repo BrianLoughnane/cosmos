30a3ba5489c663c1fa799ef03c28d446eac9091d
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-08-14 16:13:13 -0400

    More JS added.
    Everything working so far.  Needs new game option.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 // Question Class Constructor
 // =======================================
 
-function Question(question, op1, op2, op3, op4, op5, correct) {
+function Question(question, op1, op2, op3, op4, op5, correct, description) {
 	this.question = question;
 	this.op1 = op1;
 	this.op2 = op2;
@@ -14,6 +14,7 @@ function Question(question, op1, op2, op3, op4, op5, correct) {
 	this.op4 = op4;
 	this.op5 = op5;
 	this.correct = correct;
+	this.desctiption = description;
 };
 
 
@@ -24,11 +25,11 @@ function Question(question, op1, op2, op3, op4, op5, correct) {
 
 var quiz = new Array();
 
-quiz[0] = new Question("What's the tallest mountain in the world?", "Mt. Hood", "Mt. Kilimanjaro", "Mt. Sinai", "Mt. Everest", "Mt. Rushmore", "Mt. Everest")
-quiz[1] = new Question("Which number comes first?", "1", "2", "3", "4", "5", "1");
-quiz[2] = new Question("This is question number 3?", "1", "2", "3", "4", "5", "3");
-quiz[3] = new Question("This is question number 4?", "1", "2", "3", "4", "5", "4");
-quiz[4] = new Question("This is question number 5?", "1", "2", "3", "4", "5", "5");
+quiz[0] = new Question("What's the tallest mountain in the world?", "Mt. Hood", "Mt. Kilimanjaro", "Mt. Sinai", "Mt. Everest", "Mt. Rushmore", "Mt. Everest", "this is question number 1")
+quiz[1] = new Question("Which number comes first?", "1", "2", "3", "4", "5", "1", "this is question number 2");
+quiz[2] = new Question("This is question number 3?", "1", "2", "3", "4", "5", "3", "this is question number 3");
+quiz[3] = new Question("This is question number 4?", "1", "2", "3", "4", "5", "4", "this is question number 4");
+quiz[4] = new Question("This is question number 5?", "1", "2", "3", "4", "5", "5", "this is question number 5");
 
 console.log(quiz[0].question);
 
@@ -49,6 +50,73 @@ function askQuestion(qIndex) {
 	$(".question li:nth-child(5)").text(quiz[qIndex].op5);	
 	console.log("Fired askQuestion(" + qIndex + "]")
 }
+
+
+
+
+var count = 0;
+
+var score = 0;
+var quesNum = 1;
+
+askQuestion(count);
+
+$("section")
+.on("click", "li", function(event) {
+	event.stopPropagation();
+	event.preventDefault();
+	
+	var correct = quiz[count].correct;
+	var yourAnswer = $(this).text();
+
+	if(yourAnswer === correct) {
+		console.log("correct!");
+		$(".answer h1").text("Correct").css("color", "green");
+		score++;
+	} else {
+		console.log("wrong answer");
+		$(".answer h1").text("Incorrect").css("color", "red");
+	}
+
+	$(".yourAns").text(yourAnswer);
+	$(".correctAns").text(correct);
+	$(".answer p").text(quiz[count].desctiption);
+	$(".score").text(score);
+
+	$(".question").fadeOut("slow");
+	$(".answer").delay(900).fadeIn("slow");
+	// evaluateAnswer(count);
+	}
+);
+
+$("section")
+.on("click", ".next", function(event) {
+	event.stopPropagation();
+	event.preventDefault();
+
+	count++;
+
+	if(count < quiz.length) {
+		askQuestion(count);
+		quesNum++;
+		$(".quesNum").text(quesNum);
+	} else {
+		alert("the game is over");
+	}
+
+	}
+);
+
+
+
+
+// var count = 0;
+
+// for(var i=0; i<quiz.length; i++) {
+
+// 	askQuestion(count);
+
+// }
 
 // ======================================================
 // evaluateAnswer - Compares Clicked Line to Correct Line
@@ -88,55 +156,6 @@ function askQuestion(qIndex) {
 // 	askQuestion(qIndex);
 // 	evaluateAnswer(correctAns);
 // }
-
-
-
-
-var count = 0;
-
-askQuestion(count);
-
-$("section")
-.on("click", "li", function(event) {
-	event.stopPropagation();
-	event.preventDefault();
-	
-	var correct = quiz[count].correct;
-
-	if($(this).text() === correct) {
-	// if(this.innerText === correct) {
-		console.log("correct!");
-		
-
-	} else {
-		console.log("wrong answer");
-
-	}
-
-	$(".question").fadeOut("slow");
-	$(".answer").delay(900).fadeIn("slow");
-	// evaluateAnswer(count);
-	}
-);
-
-$("section")
-.on("click", ".next", function(event) {
-	event.stopPropagation();
-	event.preventDefault();
-	count++;
-	askQuestion(count);
-	}
-);
-
-// var count = 0;
-
-// for(var i=0; i<quiz.length; i++) {
-
-// 	askQuestion(count);
-
-// }
-
-
 
 
 
